37946ef0da473d40981c15b549c9322246e459f7
rifqifarelmuhammad <rifqifarel222@gmail.com>
2024-08-11 21:06:00 +0700

    feat: added product configuration for ecommerce payment gateway

---
+++ src/paymentgateway.product.ecommerce/module-info.java
@@ -0,0 +1,7 @@
+module paymentgateway.product.ecommerce {
+    requires paymentgateway.payment.core;
+
+    requires paymentgateway.payment.multicurrencysupport;
+
+    requires paymentgateway.payment.frauddetection;
+}
