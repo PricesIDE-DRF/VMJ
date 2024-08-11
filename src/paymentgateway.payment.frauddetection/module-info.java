07ab7278c51221c974dd66eee365a220ceab2b29
rifqifarelmuhammad <rifqifarel222@gmail.com>
2024-08-11 21:05:24 +0700

    feat: added payment fraud detection delta module

---
+++ src/paymentgateway.payment.frauddetection/module-info.java
@@ -0,0 +1,5 @@
+module paymentgateway.payment.frauddetection {
+    requires paymentgateway.payment.core;
+    requires java.logging;
+    exports paymentgateway.payment.frauddetection;
+}
---
+++ src/paymentgateway.payment.frauddetection/paymentgateway/payment/frauddetection/PaymentImpl.java
@@ -0,0 +1,29 @@
+package paymentgateway.payment.frauddetection;
+
+import paymentgateway.payment.core.Payment;
+import paymentgateway.payment.core.PaymentDecorator;
+
+import java.util.*;
+
+public class PaymentImpl extends PaymentDecorator {
+    public PaymentImpl(Payment record) {
+        super(record);
+    }
+
+    public void processPayment() {
+        super.record.processPayment();
+        
+        System.out.println("Detecting fraud in the transaction...");
+        Random random = new Random();
+        int randomInt = random.nextInt();
+        if (randomInt %2 == 0) {
+            System.out.println("No fraud has been detected, the transaction proceeded smoothly!");
+        } else {
+            System.out.println("Fraud has been detected, please review the transaction further!");
+        }
+    }
+    
+    public String toString() {
+        return super.toString();
+    }
+}
